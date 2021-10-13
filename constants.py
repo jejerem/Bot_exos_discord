09bc773a4c0fd4c6c7e33a116c09868e2824fffb
Jeremy <jeremyrozier2003@gmail.com>
2021-10-13 18:48:52 +0200

    Commit

--- constants.py
+++ constants.py
@@ -2,6 +2,7 @@ __author__ = 'Fredi'
 __Filename__ = 'constants'
 __Creationdate__ = '10/10/2021'
 
-YEAR_ROLE_ID = "704749928341045298"
+YEAR_ROLE_ID = 704749928341045298
+MODERATOR_ROLE_ID = 897886905084018758
 BOT_TOKEN = ""
 tuple_subjects = ("m", "p", "c", "i")
