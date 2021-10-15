7c438a47fbb4ef50fbc7adb3e68019da9b152c22
Jeremy <jeremyrozier2003@gmail.com>
2021-10-15 20:32:45 +0200

    commit

--- bot.py
+++ bot.py
@@ -8,7 +8,7 @@ from discord.ext import commands
 from constants import *
 
 # command prefix users will use to make requests.
-bot = commands.Bot(command_prefix='!')
+bot = commands.Bot(command_prefix='!', help_command=None)
 
 
 # Run when the bot is connected.
@@ -25,12 +25,22 @@ async def get(ctx, subject_name, theme, content_type, difficulty=1, year=0):
     """Command to get any exercises or test which is in the database :
     - subject_name must begins by 'm', 'p', 'c' or 'i'.
     - theme must be in the database.
-    - content_type must begins by 'e' or 'd'.
+    - content_type must begin by 'e'(exercise) or 'd'(ds) (gives automatically the correction too if there is one).
     - 1 <= difficulty <= 5.
     - 1 <= year <= 3."""
 
     # Object related to the author of the request.
     author_object = ctx.message.author
+
+    # subject_name doesn't begin by 'm', 'p', 'c' or 'i'.
+    if not subject_name[0].lower() in tuple_subjects:
+        await ctx.send(f"{author_object.mention} : I only know MPCI subjects.")
+        return
+
+    else:
+        pass
+
+    # We browse author's roles.
     for author_role in author_object.roles:
         # We found the year role => the user is in the license.
         if author_role.id == YEAR_ROLE_ID:
@@ -54,40 +64,30 @@ async def get(ctx, subject_name, theme, content_type, difficulty=1, year=0):
                        f"Come on don't underestimate yourself.")
         difficulty = 1
 
-    # subject_name doesn't begin by 'm', 'p', 'c' or 'i'.
-    if not subject_name[0].lower() in tuple_subjects:
-        await ctx.send("I only know MPCI subjects.")
-        return
-
-    else:
-        pass
-
 
 # help command
 @bot.command()
-async def help_commands(ctx):
-    await ctx.send("Available commands :\n"
-                   "- !get subject theme content_type(exo, ds) difficulty "
-                   "year(only if you are not in the license anymore)\n"
-                   "- !html : let him tell you his opinion about it.")
+async def help(ctx):
+    await ctx.send(f"""```Available commands :
+                   - !get subject theme content_type(exo, ds) difficulty
+                   year(only if you are not in the license anymore)
+                   
+                   - !html : let him tell you his opinion about it.```""")
 
 
 @bot.command()
 async def html(ctx):
-    await ctx.send("HTML is not a programming language.")
+    await ctx.send("__**HTML**__ is **not** a **programming language**.")
 
 
 @get.error
 async def on_command_error(ctx, error):
     if isinstance(error, commands.MissingRequiredArgument):
-        await ctx.send("- !get subject theme content_type(exo, ds) difficulty "
-                       "year(only if you are not in the license anymore)")
+        await ctx.send("""- !get subject theme content_type(exo, ds) difficulty "
+                       "year(only if you are not in the license anymore)""")
 
 
 print("Preparing bot...")
 
-# Bot's token.
-jeton = ""
-
 # Run the bot on the server.
-bot.run(jeton)
+bot.run(BOT_TOKEN)
