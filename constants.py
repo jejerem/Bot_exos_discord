0a9ee52323a284d6f84ed0f07c0249db69e6f44b
jerem <33176119+jejerem@users.noreply.github.com>
2022-01-20 21:57:30 +0100

    Delete constants.py

--- constants.py
+++
@@ -1,7 +0,0 @@
-__Filename__ = 'constants'
-__Creationdate__ = '10/10/2021'
-
-YEAR_ROLE_ID = 704749928341045298
-MODERATOR_ROLE_ID = 897886905084018758
-BOT_TOKEN = ""
-tuple_subjects = ("m", "p", "c", "i")
